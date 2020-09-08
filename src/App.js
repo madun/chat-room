cce4f034e818de49645756a99eb2fb13cd50b2db
ilman Manarul Qori <ilmanmanarulqori@ilmans-MacBook-Pro-2.local>
2020-09-08 21:42:54 +0700

    test new remote url

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Join from "./components/Join/";
 import Chat from "./components/Chat/";
+// import Chat from "./components/Chat/";
 
 const App = () => (
   <Router>
